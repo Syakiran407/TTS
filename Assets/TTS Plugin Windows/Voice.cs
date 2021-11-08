c33d0d575044738a64a43ba87db722f508622643
Mahmud Syakiran <mahmudsyakiran407@gmail.com>
2021-11-08 12:14:41 +0800

    Initial Commit

---
+++ Assets/TTS Plugin Windows/Voice.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpeechLib;
+
+using UnityEngine.Windows.Speech;
+
+public class Voice : MonoBehaviour
+{
+
+    private void Start()
+    {
+        SpVoice voice = new SpVoice();
+        //ISpeechObjectTokens voices = voice.GetVoices();
+        voice.Volume = 100; //value between 0-100
+        voice.Rate = 2; //speed
+        voice.Speak("Hello Everyone, my name is Kiran.", SpeechVoiceSpeakFlags.SVSFDefault);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            
+
+        }
+    }
+}
